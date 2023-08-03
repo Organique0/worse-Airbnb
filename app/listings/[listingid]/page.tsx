6c63b5ced11040fa0b856119c70dc1f14b291324
Organique0 <grabnar.luka@gmail.com>
2023-08-04 01:23:55 +0200

    reservations

--- app/listings/[listingid]/page.tsx
+++ app/listings/[listingid]/page.tsx
@@ -2,14 +2,16 @@ import getCurrentUser from "@/app/actions/getCurrentUser";
 import getListingById from "@/app/actions/getListingById";
 import EmptyState from "@/app/components/EmptyState";
 import ListingClient from "./ListingClient";
+import getReservations from "@/app/actions/getReservations";
 
 interface IParams {
-    listingid?: string;
+    listingId?: string;
 }
 
 const ListingPage = async ({ params }: { params: IParams }) => {
     const listing = await getListingById(params);
     const currentUser = await getCurrentUser();
+    const reservations = await getReservations(params);
 
     if (!listing) {
         return (<EmptyState />)
@@ -17,7 +19,7 @@ const ListingPage = async ({ params }: { params: IParams }) => {
 
     return (
         <div>
-            <ListingClient listing={listing} currentUser={currentUser} />
+            <ListingClient listing={listing} currentUser={currentUser} reservations={reservations} />
         </div>
     );
 }
