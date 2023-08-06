a11e17cb5ffe3d30fdb5b25e76478358e4f793d9
Organique0 <grabnar.luka@gmail.com>
2023-08-06 14:54:52 +0200

    removed github login

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -58,7 +58,6 @@ const LoginModal = () => {
         <div className="flex flex-col gap-4">
             <hr />
             <Button outline label="continue with Google" icon={FcGoogle} onClick={() => signIn('google')} />
-            <Button outline label="continue with Github" icon={AiFillGithub} onClick={() => signIn('github')} />
             <div className="text-neutral-500 text-center font-light">
                 <div className="flex flex-row items-center gap-2 justify-center">
                     <div>
