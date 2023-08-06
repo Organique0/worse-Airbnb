fe37c5e5d1bccf72a489d66fcc56a5d1529e0281
Organique0 <grabnar.luka@gmail.com>
2023-08-06 14:54:50 +0200

    the same user cannot have diff providers. wierd

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -11,10 +11,6 @@ import bcrypt from "bcrypt";
 export const authOptions: AuthOptions = {
     adapter: PrismaAdapter(prisma),
     providers: [
-        GithubProvider({
-        clientId: process.env.GITHUB_ID as string,
-        clientSecret: process.env.GITHUB_SECRET as string
-        }),
         GoogleProvider({
         clientId: process.env.GOOGLE_CLIENT_ID as string,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
